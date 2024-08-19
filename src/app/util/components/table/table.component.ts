c68e6040e7618cfc96bcdc14e9aec6b43ae95392
Eduardo Marreiro <eduardo4pc@outlook.com>
2024-08-19 14:55:12 -0300

    paginação e filtro na tabela

--- src/app/util/components/table/table.component.ts
+++ src/app/util/components/table/table.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { NotaFiscal } from '../../../../shared/models/nota-fiscal-model';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
-import { StatusNotaFiscal } from '../../../../shared/models/status-nota-fiscal';
 
 @Component({
   selector: 'app-table',
@@ -11,25 +9,63 @@ import { StatusNotaFiscal } from '../../../../shared/models/status-nota-fiscal';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
-  @Input() data: any;
+export class TableComponent implements OnInit {
+  @Input() data: any[] = [];
   filterText: string = '';
-  notasFiscais: NotaFiscal[] = [];
-  filteredNotasFiscais: NotaFiscal[] = [];
+  filteredNotasFiscais: any[] = [];
+  paginatedNotasFiscais: any[] = [];
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalPages: number = 1;
 
   ngOnInit() {
-    this.filteredNotasFiscais = this.notasFiscais;
-    this.filterNotasFiscais();
+    this.filteredNotasFiscais = this.data;
+    this.paginate();
   }
 
   filterNotasFiscais() {
     if (this.filterText) {
-      this.filteredNotasFiscais = this.notasFiscais.filter(notaFiscal =>
+      this.filteredNotasFiscais = this.data.filter(notaFiscal =>
         notaFiscal.nomePagador.toLowerCase().includes(this.filterText.toLowerCase()) ||
         notaFiscal.numeroIdentificacao.toLowerCase().includes(this.filterText.toLowerCase())
+        
       );
     } else {
-      this.filteredNotasFiscais = this.notasFiscais;
+      this.filteredNotasFiscais = this.data;
     }
+    this.currentPage = 1;
+    this.paginate();
+  }
+
+  paginate() {
+    this.totalPages = Math.ceil(this.filteredNotasFiscais.length / this.pageSize);
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedNotasFiscais = this.filteredNotasFiscais.slice(startIndex, endIndex);
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginate();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginate();
+    }
+  }
+
+  goToPage(page: number) {
+    if (page > 0 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.paginate();
+    }
+  }
+
+  get pages() {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
   }
 }
